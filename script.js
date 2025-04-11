4525c391ee2c13542b8e89fbbb9b5a7027c3a68f
RAX <astraltechh@gmail.com>
2025-04-10 20:28:40 -0500

    wave
    I needa hide ts

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ document.getElementById("left-arrow").addEventListener("click", function() {
     document.getElementById("onboarding").style.transform = "translateX(0)";
     document.getElementById("profile").style.transform = "translateX(100%)";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const editButton = document.querySelector(".btn");
+  
+    editButton.addEventListener("click", () => {
+      editButton.classList.add("clicked");
+  
+      
+      setTimeout(() => {
+        editButton.classList.remove("clicked");
+      }, 300);
+    });
+  });
